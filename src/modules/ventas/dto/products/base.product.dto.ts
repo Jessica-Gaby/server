5870f499648df4ca5348a10b6daff84adb31382d
SANTIAGO GUALLICHICO <sdq.guallichico@yavirac.edu.ec>
2023-02-02 15:25:59 -0500

    jueves

--- src/modules/ventas/dto/products/base.product.dto.ts
+++ src/modules/ventas/dto/products/base.product.dto.ts
@@ -1,19 +1,35 @@
-import { IsNotEmpty } from "class-validator";
-import { IsNumber, IsNumberString, IsPositive, IsString } from "class-validator/types/decorator/decorators";
-import { IsNotEmptyValidationOptions, IsStringValidationOptions, IsNumberValidationOptions,IsPositiveValidationOptions} from "shared/validation";
+import { ArrayNotEmpty, IsArray, IsInt, IsPositive, IsString, IsNumber, IsNotEmpty} from "class-validator/types/decorator/decorators";
+import { isNotEmptyValidationOptions, 
+    isStringValidationOptions, 
+    isNumberValidationOptions,
+    isPositiveValidationOptions, 
+    isIntValidationOptions, 
+    isArrayValidationOptions, 
+    arrayValidationOptions} from "../../shared/validation.ts/validation-message.ts";
+import { CategoryEntity } from "../../entities/category.entity";
 
 export class BaseProductDTO{
-    @IsNotEmpty(IsNotEmptyValidationOptions())
-    @IsString (IsStringValidationOptions())
+    @IsNotEmpty(isNotEmptyValidationOptions())
+    @IsString (isStringValidationOptions())
     readonly title:string;
 
-    @IsNotEmpty(IsNotEmptyValidationOptions())
-    @IsNumber(IsNumberValidationOptions())
-    @IsPositive(IsPositiveValidationOptions())
+    @IsNotEmpty(isNotEmptyValidationOptions())
+    @IsNumber(isNumberValidationOptions())
+    @IsPositive(isPositiveValidationOptions())
     readonly price:number;
 
-    @IsNotEmpty(IsNotEmptyValidationOptions())
-    @IsString (IsStringValidationOptions())
+    @IsNotEmpty(isNotEmptyValidationOptions())
+    @IsString (isStringValidationOptions())
     readonly descripcion:string;
+
+    @IsNotEmpty(isNotEmptyValidationOptions())
+    @IsInt(isIntValidationOptions())
+    @IsPositive(isPositiveValidationOptions())
+    readonly categoryId:CategoryEntity;
+
+
+    @IsArray(isArrayValidationOptions())
+    @ArrayNotEmpty(arrayValidationOptions())
+    readonly images:string[];
 
 }

dd832e38edce24964e7abbd6fa1f52be340ff7d7
chenaoh <jgm.ayala@yavirac.edu.ec>
2023-02-02 13:07:22 -0500

    miercoles

--- src/modules/ventas/dto/products/base.product.dto.ts
+++ src/modules/ventas/dto/products/base.product.dto.ts
@@ -1,11 +1,19 @@
 import { IsNotEmpty } from "class-validator";
-import { IsString } from "class-validator/types/decorator/decorators";
-import { IsNotEmptyValidationOptions, IsStringValidationOptions} from "shared/validation";
+import { IsNumber, IsNumberString, IsPositive, IsString } from "class-validator/types/decorator/decorators";
+import { IsNotEmptyValidationOptions, IsStringValidationOptions, IsNumberValidationOptions,IsPositiveValidationOptions} from "shared/validation";
 
 export class BaseProductDTO{
     @IsNotEmpty(IsNotEmptyValidationOptions())
-    @IsString (IsStringValidationOptions)
-    readonly title;
+    @IsString (IsStringValidationOptions())
+    readonly title:string;
 
-    
+    @IsNotEmpty(IsNotEmptyValidationOptions())
+    @IsNumber(IsNumberValidationOptions())
+    @IsPositive(IsPositiveValidationOptions())
+    readonly price:number;
+
+    @IsNotEmpty(IsNotEmptyValidationOptions())
+    @IsString (IsStringValidationOptions())
+    readonly descripcion:string;
+
 }
